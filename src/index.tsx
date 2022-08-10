1dd85ef4fa98a9867d3c0f57f56e1f7d3e850f24
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-10 15:29:11 -0300

    feat(index): import react-toastify and styles SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "react-toastify/dist/ReactToastify.css";
+import "./styles/styles.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
