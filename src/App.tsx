967a8e16a9ac2b41c740ac0fad47a2b6ce1cb552
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-10 16:40:05 -0300

    feat(app): import components and create messages connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import io from "socket.io-client";
+import ConnectedUsers from "./components/ConnectedUsers/ConnectedUsers";
+import EnterUsername from "./components/EnterUsername";
 
 function App() {
   const [connected, setConnected] = useState(false);
@@ -8,6 +10,10 @@ function App() {
   const [connectedUsers, setConnectedUsers] = useState(
     [] as { id: string; username: string }[]
   );
+  const [messages, setMessages] = useState(
+    [] as { message: string; username: string }[]
+  );
+  const [message, setMessage] = useState("");
 
   const socketClient = useRef<SocketIOClient.Socket>();
 
@@ -29,11 +35,17 @@ function App() {
           setConnectedUsers(
             ConnectedUsers.filter((user) => user.username !== username)
           );
-          console.log(ConnectedUsers);
+        }
+      );
+
+      socketClient.current.on(
+        "receive-message",
+        (message: { message: string; username: string }) => {
+          setMessages((prev) => [...prev, message]);
         }
       );
     }
-  }, []);
+  }, [username]);
 
   const handleConnection = () => {
     if (socketClient.current) {
@@ -41,32 +53,24 @@ function App() {
     }
   };
 
+  const handleSendMessage = () => {
+    
+  }
+
   return (
     <div className="app">
       {!connected && (
-        <form
-          className="enter-username-form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleConnection();
-          }}
-        >
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter your username..."
-            required={true}
-          />
-
-          <button type="submit">Submit</button>
-        </form>
+        <EnterUsername
+          handleConnection={handleConnection}
+          username={username}
+          setUsername={setUsername}
+        />
       )}
 
       {connected && (
-        <div>
-          <h2>Connected</h2>
-        </div>
+        <>
+          <ConnectedUsers ConnectedUsers={connectedUsers} />
+        </>
       )}
       <ToastContainer position="bottom-right" />
     </div>
